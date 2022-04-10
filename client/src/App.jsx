bdec089b1e9d0b85ba33c73e150b92cf4e3cfab6
afahgan <afahgan@students.iugaza.edu.ps>
2022-04-10 21:10:19 +0300

    Create-Home #8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,45 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Home from './components/Home/home';
+import Ad from './components/Ad/Ad';
 
  const App = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
+    // <Router>
+    //   <Header />
+    //   <Switch>
+    //     <Route exact path='/'>
+    //       <Home />
+    //       <div>
+    //         Hi
+    //       </div>
+    //     </Route>
 
-        <Route path='/login'>
-          <Login />
-        </Route>
+    //     <Route path='/login'>
+    //       <Login />
+    //     </Route>
 
-        <Route exact path='/cart'>
-          <Cart />
-        </Route>
+    //     <Route exact path='/cart'>
+    //       <Cart />
+    //     </Route>
 
-        <Route exact path='/product'>
-          <Product />
-        </Route>
+    //     <Route exact path='/product'>
+    //       <Product />
+    //     </Route>
 
-        <Route exact path={'/product/:id'}>
-          <ProductInfo />
-        </Route>
+    //     <Route exact path={'/product/:id'}>
+    //       <ProductInfo />
+    //     </Route>
 
-      </Switch>
-      <Footer />
-    </Router>
+    //   </Switch>
+    //   <Footer />
+    // </Router>
+    <div>
+      <Home />
+    
+      <Ad />
+    </div>
   );
 }
 
---
+++ client/src/components/Home/home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./home.css";
+function home() {
+  return (
+    <div className="ss">
+      <header className="home-header">
+        <div className="header-info">
+          <div className="discount">
+            <h6 className="title">Lorem ipsum dolor sit amet, consetetur</h6>
+           
+            <p className ="content">
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            </p>
+           
+
+            <button>Buy Now</button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default home;

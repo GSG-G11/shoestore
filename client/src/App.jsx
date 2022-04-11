c8bf9b2d3f0af3cea13ea03e62dad2d08c49b3e1
nabilramy <nabil.abushaban@gmail.com>
2022-04-11 22:42:06 +0300

    modify the react-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,37 @@
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./App.css";
-import Logo from "./components/header/upperHeader/Logo";
-import SearchBar from "./components/header/upperHeader/SearchBar";
-import CartIcon from "./components/header/upperHeader/CartIcon";
-import LoginButton from "./components/header/upperHeader/LoginButton";
-import "./components/header/upperHeader/upperHeader.css";
-import Dropdwon from "./components/header/downHeader/Dropdown";
-import NavigationMenu from "./components/header/downHeader/NavigationMenu";
-
-import "./App.css";
-import Home from "./components/Home/home";
-import Ad from "./components/Ad/Ad";
-
+import Home from "./pages/home";
+import Header from "./components/header";
 const App = () => {
   return (
-    // <Router>
-    //   <Header />
-    //   <Switch>
-    //     <Route exact path='/'>
-    //       <Home />
-    //       <div>
-    //         Hi
-    //       </div>
-    //     </Route>
+    <>
+      <Header />
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
 
-    //     <Route path='/login'>
-    //       <Login />
-    //     </Route>
+        {/* <Route path='/login'>
+          <Login />
+        </Route>
 
-    //     <Route exact path='/cart'>
-    //       <Cart />
-    //     </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
 
-    //     <Route exact path='/product'>
-    //       <Product />
-    //     </Route>
+        <Route exact path='/product'>
+          <Product />
+        </Route>
 
-    //     <Route exact path={'/product/:id'}>
-    //       <ProductInfo />
-    //     </Route>
+        <Route exact path={'/product/:id'}>
+          <ProductInfo />
+        </Route> */}
 
-    //   </Switch>
-    //   <Footer />
-    // </Router>
-    <div>
-      <div className="App">
-        <div className="Header">
-          <div className="upper__header">
-            <Logo />
-            <SearchBar />
-            <CartIcon />
-            <LoginButton />
-          </div>
-          <div className="down__header">
-            <Dropdwon />
-            <NavigationMenu />
-          </div>
-        </div>
-      </div>
-      <Home />
-      <Ad />
-    </div>
+      </Switch>
+    </Router>
+      {/* <Footer /> */}
+    </>
   );
 };
 

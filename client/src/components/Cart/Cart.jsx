898d6adba7aae03f615584632a0b4d1651074059
amasri88 <amran.man99@gmail.com>
2022-04-12 02:56:17 +0300

    handle cart products

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,11 +1,4 @@
-import Logo from "../header/upperHeader/Logo";
-import SearchBar from "../header/upperHeader/SearchBar";
-import CartIcon from "../header/upperHeader/CartIcon";
-import LoginButton from "../header/upperHeader/LoginButton";
-import "../header/upperHeader/upperHeader.css";
-import Dropdwon from "../header/downHeader/Dropdown";
-import NavigationMenu from "../header/downHeader/NavigationMenu";
-import imgTest from './Screenshot from 2022-04-11 16-25-35.png'
+
 import './Cart.css'
 
 
@@ -13,60 +6,66 @@ import './Cart.css'
 
 
 const Cart = () => {
-  return (
+
+  const products = JSON.parse(localStorage.getItem('products')) || [];
+
+  // const products = [{ name: 'amran', image: 'https://i.ibb.co/QcHvYvf/back-Ground-header.jpg', price: 2000, description: 'pla plapla' },
+  // { name: 'amran', image: 'https://i.ibb.co/QcHvYvf/back-Ground-header.jpg', price: 1000, description: 'pla plapla' }];
+
+
+  const result = products.map((product) => (
     <div>
-      <div className="Header">
-        <div className="upper__header">
-          <Logo />
-          <SearchBar />
-          <CartIcon />
-          <LoginButton />
-        </div>
-        <div className="down__header">
-          <Dropdwon />
-          <NavigationMenu />
+      <div className="content__order">
+        <div className="img__product"> <img src={product.image} alt="product" /></div>
+
+        <div className="tools">
+
+          <div className="product__name__description">
+
+            <p> {product.name} </p>
+            <p className="product__description"> {product.description} </p>
+
+          </div>
+          <div className="tools2">
+            <div className="minus__sign"><i class="fa-solid fa-minus"></i></div>
+            <p > 1 </p>
+            <div className="plus__sign"> <i class="fa-solid fa-plus"></i></div>
+            <div className="price__product">${product.price}</div>
+            <div className="remove__icon"> <i class="fa-solid fa-trash-can"></i></div>
+          </div>
         </div>
       </div>
+      <hr className="hrr" />
+    </div>
+
+
+  ))
+
+  const prices = products.map(product => {
+    return +product.price;
+  });
+
+  const totalPrice = prices.reduce((acc, curr) => acc + curr, 0);
 
 
 
+  return (
+
+
+    <div>
 
 
       <div className="order_summary">
-
-      <p>Order summary</p>
-
-
+        <p>Order summary</p>
         <p className="remove_all">remove all</p>
-
-        </div>
-
-      <div className="content__order">
-        <div className="img__product"> <img src={imgTest} alt="product" /></div>
-
-        <div className="tools">
-          <div className="product__name"> <p> product name </p></div>
-
-          <div className="minus__sign"><i class="fa-solid fa-minus"></i></div>
-          <p > 1 </p>
-          <div className="plus__sign"> <i class="fa-solid fa-plus"></i></div>
-          <div className="price__product">$19.32</div>
-          <div className="remove__icon"> <i class="fa-solid fa-trash-can"></i></div>
-
-        </div>
-
-
-
-
-
       </div>
 
-        <hr  className="hrr"/>
+      {result.length ?  result : <li className="no__product"> no product in your cart </li> }
 
-        <div className="total_price__cont">
-          <div className="text__total">Total (including tax)</div>
-          <div className="total_price">$19.32</div>
-        </div>
+      <div className="total_price__cont">
+        <div className="text__total">Total (including tax)</div>
+        <div className="total_price">${totalPrice}</div>
+      </div>
 
     </div>
   )

264a8b586576c719ca818a47847867599ce95445
amasri88 <amran.man99@gmail.com>
2022-04-12 16:05:04 +0300

    handle search to store in state

--- client/src/components/header/upperHeader/SearchBar.jsx
+++ client/src/components/header/upperHeader/SearchBar.jsx
@@ -4,13 +4,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default class SearchBar extends React.Component {
+  
   render() {
+    const {handleSearch} = this.props;
+
     return (
       <div className="main__search__bar__wrapper">
         <input
           className="main__search__bar"
           type="text"
           placeholder="Search For Products, Brands & Categories"
+          onChange={(e) => handleSearch(e)}
+      
         />
         <FontAwesomeIcon
         className="search__icon"
